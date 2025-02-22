f0a3ad1ff398b11860426a2b390968fb96788b59
Adrian Gidaro <adriangidaro@gmail.com>
2025-02-22 11:39:42 +1100

    Implemented delete one student

--- blueprints/students_bp.py
+++ blueprints/students_bp.py
@@ -10,7 +10,7 @@ students_bp = Blueprint('students', __name__)
 # Read all - GET /students
 @students_bp.route('/students')
 def get_all_students():
-    stmt = db.select(Student)
+    stmt = db.select(Student).order_by(Student.id.asc()) # Orders by id ascending
     students = db.session.scalars(stmt)
     return many_students.dump(students)
 
@@ -44,12 +44,38 @@ def create_student():
     except IntegrityError as err:
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error": "Email address already in use."}, 409 # Conflict
-        elif err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
-            return {"error": str(err.orig)}, 406 # Not acceptable
+        # elif err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+        #     return {"error": str(err.orig)}, 406 # Not acceptable
         else:
             return {"error": err.orig.diag.message_detail}, 400
 
 # Update - PUT /students/<int:student_id>
+@students_bp.route('/students/<int:student_id>', methods=['PUT', 'PATCH'])
+def update_student(student_id):
+    try:
+        # Fetch student by id
+        stmt = db.select(Student).filter_by(id=student_id)
+        student = db.session.scalar(stmt)
+        if student:
+            # Get incoming request body (JSON)
+            data = student_without_id.load(request.json)
+            # Update the attributes of the student with the incoming data
+            # OR option means that the below code covers both PUT and PATCH methods
+            student.name = data.get('name') or student.name
+            student.email = data.get('email') or student.email
+            student.address = data.get('address') or student.address
+            # Commit the instance to the db
+            db.session.commit()
+            return one_student.dump(student), 200
+        else:
+            return {"error": f"Student with id {student_id} not found."}, 404
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {"error": "Email address already in use."}, 409 # Conflict
+        # elif err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+        #     return {"error": str(err.orig)}, 400
+        else:
+            return {"error": err.orig.diag.message_detail}, 400
 
 # Delete - DELETE /students/<int:student_id>
 @students_bp.route('/students/<int:student_id>', methods=['DELETE'])
